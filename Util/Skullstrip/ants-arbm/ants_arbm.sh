cd0a14f1b8dcc1809436104106d0c5655a7075ec
Torgil Vangberg <torgil.vangberg@gmail.com>
2020-08-11 15:46:43 +0200

    added command line parsing and more accurate two-step  warping

--- Util/Skullstrip/ants-arbm/ants_arbm.sh
+++ Util/Skullstrip/ants-arbm/ants_arbm.sh
@@ -1,86 +1,139 @@
 #!/bin/bash
-
 # arbm method with ants
+# NOTE! the input images benefit from N4 bias correction before skull strip
+# Usage
+# ants_arbm.sh -i t1-file -d studyDir -m brain_mask_name
+# T1NAME=mp2rage_corrected.nii
+# SDIR=/home/torgil/Projects/WMH-PET/Testing/mp2rage_skullstrip/Data/test2/
+# brainMaskName=brainmask.nii.gz
 
-T1NAME=t1w.nii.gz
-TEMPLATE=/home/torgil/Git/Util/Skullstrip/arbm/templates/MNI152_T1_1mm.nii.gz
-TMASK=/home/torgil/Git/Util/Skullstrip/ants-arbm/templates/mask_for_ARBM.nii.gz
-DMASK=/home/torgil/Git/Util/Skullstrip/ants-arbm/templates/dil_20.nii.gz
-ncores=12
-export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=$ncores
-SDIR=/home/torgil/Projects/Anorexia/3T/img/
-brainMaskName=brainmask.nii.gz
+# parse inputs 
+#
+# test for empty args
+if [ $# -eq 0 ] 
+    then
+      usage
+      exit 2
+fi
+
+
+# delete temp files?
+cleanup=TRUE
+# parse args
+
+while getopts "i:d:m:" flag
+do
+  case "$flag" in
+    i)
+      T1NAME=$OPTARG
+      ;;
+    d)
+      SDIR=$OPTARG
+      ;;
+    m)
+      brainMaskName=$OPTARG
+      ;;
+    h|?)
+      echo $flag
+      usage
+      exit 2
+      ;;
+  esac
+done
+# get path
+CODE=$(dirname $(readlink -f $0))
+TEMPLATE=$FSL_DIR/data/standard/MNI152_T1_1mm.nii.gz
+TEMPLATE_BRAIN=$FSL_DIR/data/standard/MNI152_T1_1mm_brain.nii.gz
+TEMPLATE_HUNT=${CODE}/templates/N32-T1-template.nii.gz
+#TMASK=${CODE}/templates/mask_for_ARBM.nii.gz
+TMASK=${CODE}/templates/new_arbm_mask.nii.gz
+templMask=${CODE}/templates/dil_20.nii.gz
+ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=5
+
 
 # compute template -> T1 transform
 # loop over study dir and find target
-T1Files=(`find $SDIR -name $T1NAME -type f`)
+T1Files=$(find $SDIR -name $T1NAME -type f)
+
 for MOVING in ${T1Files[@]}; do    
     imDir=$(dirname $MOVING)
-    OUT=${imDir}/t1_to_template
+    OUT=${imDir}/t1_to_template_quick
+    # calculate native -> MNI warp
+    antsRegistrationSyNQuick.sh -d 3\
+     				-n $ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS \
+     				-j 0 \
+     				-f $TEMPLATE \
+     				-m $MOVING \
+     				-t s \
+     				-o $OUT    
 
-    antsRegistrationSyN.sh -d 3\
-    				-n $ncores \
-    				-j 1 \
-    				-f $TEMPLATE \
-    				-m $MOVING \
-				-x $DMASK \
-    				-o $OUT
-    
-    # warp wmh 
+
+    # warp MNI -> native 
+    flMask=${imDir}/floatMask.nii.gz   
     antsApplyTransforms -d 3\
-			-i $TMASK\
-			-r $MOVING\
-			-o ${imDir}/${brainMaskName}\
-			-t [${OUT}0GenericAffine.mat,1]\
-    			-t ${OUT}1InverseWarp.nii.gz\
-			-n NearestNeighbor
+			-i $TMASK \
+			-r $MOVING \
+			-o $flMask \
+			-t [${OUT}0GenericAffine.mat,1] \
+    			-t ${OUT}1InverseWarp.nii.gz \
+			-n Linear
+
+    # name of binary brainmask
+    brainMask=${imDir}/${brainMaskName}
+    fslmaths $flMask -thr 0.1 -bin $brainMask
     
-    # mask brain		     
-    fslmaths $MOVING -mul ${imDir}/${brainMaskName}  ${imDir}/t1w_brain.nii.gz
+    # mask brain
+    ssBrain=${imDir}/t1w_brain_0.nii.gz
+    fslmaths $MOVING -mul $brainMask  $ssBrain
+
+    # now run a more accurate mni_brain -> native_brain transform
+    warpName=${imDir}/t1_to_template
+    moving=$ssBrain
+    target=$TEMPLATE_HUNT
+    antsRegistration --verbose 1\
+		 --dimensionality 3\
+		 --float 0\
+		 --masks [$brainMask,$templMask] \
+ 		 --output [${warpName},${warpName}Warped.nii.gz,${warpName}InverseWarped.nii.gz]\
+		 --interpolation Linear\
+		 --use-histogram-matching 0\
+		 --winsorize-image-intensities [0.005,0.995]\
+		 --initial-moving-transform [${target},${moving},1]\
+		 --transform Rigid[0.1]\
+		 --metric MI[${target},${moving},1,32,Regular,0.25]\
+		 --convergence [1000x500x250x10,1e-6,10]\
+		 --shrink-factors 8x4x2x1\
+		 --smoothing-sigmas 3x2x1x0vox\
+		 --transform Affine[0.1]\
+		 --metric MI[${target},${moving},1,32,Regular,0.25]\
+		 --convergence [1000x500x250x10,1e-6,10]\
+		 --shrink-factors 8x4x2x1\
+		 --smoothing-sigmas 3x2x1x0vox\
+		 --transform SyN[0.1,3,0]\
+		 --metric CC[${target},${moving},1,4]\
+		 --convergence [100x70x50x10,1e-6,10]\
+		 --shrink-factors 8x4x2x1\
+		 --smoothing-sigmas 3x2x1x0vox
+
+
+     antsApplyTransforms -d 3\
+			-i $TMASK \
+			-r $MOVING \
+			-o $brainMask \
+			-t [${warpName}0GenericAffine.mat,1] \
+    			-t ${warpName}1InverseWarp.nii.gz \
+			-n Linear
+
+     # correct boundary of mask
+     fslmaths $brainMask -thr 0.5 -bin $brainMask
+
+     
+     t1Brain=${imDir}/t1w_brain.nii.gz
+     fslmaths $MOVING -mul $brainMask  $t1Brain
+
+
+     if [ $cleanup == TRUE ]; then
+	 rm $ssBrain $flMask ${imDir}/t1_to_template_quick*
+     fi
+     
 done
-
-
-# code for using masks , however, this does not work with current binary release
-# need to compile code
-
-    # movingMask=${imDir}/bm.nii.gz
-    # tmp1=${imDir}/tmp1.nii.gz
-    # tmp2=${imDir}/tmp2.nii.gz
-    # echo "***** mri_watershed ********"
-    # mri_watershed $MOVING $tmp1
-    # echo "*****  bet ********"
-    # bet $tmp1 $tmp2 -f 0.2 -m # prduce mask file tmp2_mask.nii.gz
-    # betMask=${imDir}/tmp2_mask.nii.gz
-    # movingMask=${imDir}/brainmaskDil20.nii.gz
-    # echo "*****  ImageMath ********"
-    # ImageMath 3 $movingMask MD $betMask  20
-    # rm $tmp1 $tmp2 $betMask
-
-
-
-    # antsRegistration --verbose 1\
-    # 		     --dimensionality 3\
-    # 		     --float 0\
-    #                  --output [${OUT},${OUT}Warped.nii.gz,${OUT}InverseWarped.nii.gz]\
-    # 		     --interpolation Linear\
-    # 		     --use-histogram-matching 1\
-    # 		     --winsorize-image-intensities [0.005,0.995]\
-    # 		     --initial-moving-transform ["$TEMPLATE","$MOVING",1]\
-    # 		     --transform Rigid[0.1]\
-    # 		     --masks [NULL,NULL]\
-    # 		     --metric MI["$TEMPLATE","$MOVING",1,32,Regular,0.25]\
-    # 		     --convergence [1000x500x250x100,1e-6,10]\
-    # 		     --shrink-factors 8x4x2x1\
-    # 		     --smoothing-sigmas 3x2x1x0vox\
-    # 		     --transform Affine[0.1]\
-    # 		     --masks [NULL,NULL]\
-    #                  --metric MI["$TEMPLATE","$MOVING",1,32,Regular,0.25]\
-    # 		     --convergence [1000x500x250x100,1e-6,10]\
-    # 		     --shrink-factors 8x4x2x1\
-    # 		     --smoothing-sigmas 3x2x1x0vox\
-    # 		     --transform SyN[0.1,3,0]\
-    # 		     --masks [$fixedMask,$movingMask]\
-    # 		     --metric CC["$TEMPLATE","$MOVING",1,4]\
-    # 		     --convergence [100x70x50x20,1e-6,10]\
-    # 		     --shrink-factors 8x4x2x1
-
